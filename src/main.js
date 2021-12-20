0bb891cdb4212c7729deb96e002b1a9328dfacac
hailita1 <hailit2306@gmail.com>
2021-12-20 22:29:40 +0700

    update code

--- src/main.js
+++ src/main.js
@@ -5,7 +5,13 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 import "@fortawesome/fontawesome-free/css/all.css"
 import "@fortawesome/fontawesome-free/js/all.js"
+import {BootstrapVue, BootstrapVueIcons} from 'bootstrap-vue'
 
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
 Vue.config.productionTip = false
 
 new Vue({
--- src/views/Checklist.vue
+++ src/views/Checklist.vue
@@ -13,120 +13,34 @@
       </tr>
       </thead>
       <tbody>
-      <tr class="row">
-        <th class="col-1" scope="row">1</th>
-        <td class="col-6">What is your name ?</td>
+      <tr class="row" :key="checklist.id" v-for="(checklist, index) in listChecklist">
+        <th class="col-1" scope="row">{{ index + 1 }}</th>
+        <td class="col-6">{{ checklist.content }}</td>
         <td class="col-2">
           <div class="row">
             <div class="col-3"></div>
-            <div class="col-6 ">
-              <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-                <option value="OK">OK</option>
-                <option value="NG">NG</option>
-                <option value="N/A">N/A</option>
-              </select>
+            <div class="col-6">
+              <div>
+                <b-form-select
+                    v-model="checklist.result"
+                    :options="checklist.results"
+                    class="mb-3"
+                    value-field="id"
+                    text-field="value"
+                    @change=handleChange($event,checklist)
+                ></b-form-select>
+              </div>
             </div>
             <div class="col-3"></div>
           </div>
-
         </td>
         <td class="col-3">
           <div class="row">
             <div class="col-3"></div>
             <div class="col-2" style="margin-left: auto; margin-right: auto">
-              <button class="btn btn-block btn-warning"
+              <button class="btn btn-block btn-warning" @click="update(listChecklist)"
                       style="color: white; width: 44px; height: 38px">
                 <i class="fas fa-edit"></i>
-              </button>
-            </div>
-            <div class="col-2" style="margin-left: auto; margin-right: auto">
-              <button class="btn btn-block btn-primary"
-                      style="color: white; width: 44px; height: 38px">
-                <i class="fas fa-info"></i>
-              </button>
-            </div>
-            <div class="col-2" style="margin-left: auto; margin-right: auto">
-              <button type="button" class="btn btn-block btn-danger"
-                      style="color: white; width: 44px; height: 38px">
-                <i class="fas fa-trash"></i>
-              </button>
-            </div>
-            <div class="col-3"></div>
-          </div>
-        </td>
-      </tr>
-      <tr class="row">
-        <th class="col-1" scope="row">2</th>
-        <td class="col-6">What is your name ?</td>
-        <td class="col-2">
-          <div class="row">
-            <div class="col-3"></div>
-            <div class="col-6 ">
-              <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-                <option value="OK">OK</option>
-                <option value="NG">NG</option>
-                <option value="N/A">N/A</option>
-              </select>
-            </div>
-            <div class="col-3"></div>
-          </div>
-
-        </td>
-        <td class="col-3">
-          <div class="row">
-            <div class="col-3"></div>
-            <div class="col-2" style="margin-left: auto; margin-right: auto">
-              <button class="btn btn-block btn-warning"
-                      style="color: white; width: 44px; height: 38px">
-                <i class="fas fa-edit"></i>
-              </button>
-            </div>
-            <div class="col-2" style="margin-left: auto; margin-right: auto">
-              <button class="btn btn-block btn-primary"
-                      style="color: white; width: 44px; height: 38px">
-                <i class="fas fa-info"></i>
-              </button>
-            </div>
-            <div class="col-2" style="margin-left: auto; margin-right: auto">
-              <button type="button" class="btn btn-block btn-danger"
-                      style="color: white; width: 44px; height: 38px">
-                <i class="fas fa-trash"></i>
-              </button>
-            </div>
-            <div class="col-3"></div>
-          </div>
-        </td>
-      </tr>
-      <tr class="row">
-        <th class="col-1" scope="row">3</th>
-        <td class="col-6">What is your name ?</td>
-        <td class="col-2">
-          <div class="row">
-            <div class="col-3"></div>
-            <div class="col-6 ">
-              <select class="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
-                <option value="OK">OK</option>
-                <option value="NG">NG</option>
-                <option value="N/A">N/A</option>
-              </select>
-            </div>
-            <div class="col-3"></div>
-          </div>
-
-        </td>
-        <td class="col-3">
-          <div class="row">
-            <div class="col-3"></div>
-            <div class="col-2" style="margin-left: auto; margin-right: auto">
-              <button class="btn btn-block btn-warning"
-                      style="color: white; width: 44px; height: 38px">
-                <i class="fas fa-edit"></i>
-              </button>
-            </div>
-            <div class="col-2" style="margin-left: auto; margin-right: auto">
-              <button class="btn btn-block btn-primary"
-                      style="color: white; width: 44px; height: 38px">
-                <i class="fas fa-info"></i>
               </button>
             </div>
             <div class="col-2" style="margin-left: auto; margin-right: auto">
@@ -145,8 +59,56 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
-  name: "Checklist"
+  data() {
+    return {
+      listChecklist: [],
+      listResult: [],
+      errors: [],
+      selected: ''
+    }
+  },
+  // lấy dữ liệu khi component được tạo thành công
+  created() {
+    this.getAllChecklist();
+    axios.get(`http://localhost:9000/api/result`)
+        .then(response => {
+          this.listResult = response.data;
+        })
+        .catch(e => {
+          this.errors.push(e)
+        })
+  },
+  methods: {
+    handleChange($event, checklist) {
+      checklist.result = $event;
+    },
+    update(checklist) {
+      console.log(checklist);
+      axios.put(`http://localhost:9000/api/checklist`, checklist)
+          .then(response => {
+            if (response) {
+              this.getAllChecklist();
+              console.log("Update thành công")
+            }
+          })
+          .catch(e => {
+            this.errors.push(e)
+          })
+    },
+    getAllChecklist() {
+      axios.get(`http://localhost:9000/api/checklist`)
+          .then(response => {
+            this.listChecklist = response.data;
+            console.log(this.listChecklist)
+          })
+          .catch(e => {
+            this.errors.push(e)
+          })
+    }
+  }
 }
 </script>
 
